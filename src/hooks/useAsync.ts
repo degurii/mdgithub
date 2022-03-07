81a05cb4e5dd9aa8d3c7b4f00f1d122efd56c59d
degurii <sansam709@gmail.com>
2022-03-07 22:33:32 +0900

    feat: useAsync 구현
    * useAsync는 비동기 함수를 받아 {loading, data, error} 형태의 값을 반환함
    
    * 요청 결과에 따라 각 프로퍼티를 업데이트한다.

---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,70 @@
+import React, { useCallback, useEffect, useReducer } from 'react';
+import type { DependencyList } from 'react';
+
+const enum ActionType {
+  LOADING = 'LOADING',
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR',
+}
+
+type Action<T> =
+  | { type: ActionType.LOADING }
+  | { type: ActionType.SUCCESS; payload: T }
+  | { type: ActionType.ERROR; payload: Error };
+
+type State<T> = {
+  loading: boolean;
+  data?: T;
+  error?: Error;
+};
+
+const createReducer =
+  <T>() =>
+  (prevState: State<T>, action: Action<T>): State<T> => {
+    switch (action.type) {
+      case ActionType.LOADING:
+        return { loading: true };
+      case ActionType.SUCCESS:
+        return {
+          loading: false,
+          data: action.payload,
+        };
+      case ActionType.ERROR:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+      default:
+        return prevState;
+    }
+  };
+
+export const useAsync = <T extends (...args: any[]) => Promise<any>>(
+  fn: T,
+  deps: DependencyList = [],
+): [State<Awaited<ReturnType<T>>>, () => void] => {
+  const [state, dispatch] = useReducer(
+    createReducer<Awaited<ReturnType<T>>>(),
+    {
+      loading: false,
+    },
+  );
+
+  const fetchData = useCallback(() => {
+    dispatch({ type: ActionType.LOADING });
+    fn()
+      .then((result) =>
+        dispatch({
+          type: ActionType.SUCCESS,
+          payload: result,
+        }),
+      )
+      .catch((error) => dispatch({ type: ActionType.ERROR, payload: error }));
+  }, deps);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return [state, fetchData];
+};
