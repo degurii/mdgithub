a5235cd461d89fb1e6845bc3dbf016d7378f995d
degurii <sansam709@gmail.com>
2022-03-08 01:02:48 +0900

    feat: Home 페이지 구현
    * 루트 컴포넌트인 App에서 inputUrl 스테이트를 내려 받아 저장함
    
    * 사용자가 url을 입력하면 호스트가 github.com인지 간단히 검증 후 TIL 페이지로 네비게이팅함

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import RepositoryBlog from './pages/RepositoryBlog';
+import TIL from './pages/TIL';
 import NotFound from './pages/NotFound';
 
 function App() {
+  const [inputUrl, setInputUrl] = useState('');
+  const handleChangeInputUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputUrl(e.target.value);
+  };
   return (
     <Routes>
       <Route path="/">
-        <Route index element={<Home />} />
-        <Route path=":owner/:repo/*" element={<RepositoryBlog />} />
+        <Route
+          index
+          element={
+            <Home
+              inputUrl={inputUrl}
+              handleChangeInputUrl={handleChangeInputUrl}
+            />
+          }
+        />
+        {/* <Route path="login" element={<Login />} /> */}
+        <Route path=":owner/:repo/*" element={<TIL />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { removeHostFromGithubUrl } from '../../utils/url';
 
-function Home(props: any) {
-  console.log(props);
-  return <div>Home!!</div>;
+import InputBox from '../../components/InputBox';
+
+type Props = {
+  inputUrl: string;
+  handleChangeInputUrl: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+function Home({ inputUrl, handleChangeInputUrl }: Props) {
+  const navigate = useNavigate();
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const url = removeHostFromGithubUrl(inputUrl);
+      if (url === null) {
+        navigate('404-not-found');
+      } else {
+        navigate(url);
+      }
+    }
+  };
+  return (
+    <div className="relative pt-36 pb-16 sm:pb-24">
+      <main className="mt-16 mx-auto px-4 ">
+        <h1 className="text-center text-5xl sm:text-7xl tracking-tight font-extrabold text-gray-900">
+          <span className="block select-none">
+            Github <span className="text-indigo-500">TIL</span>
+          </span>
+        </h1>
+        <div className="mt-9 sm:mt-14 relative flex items-center justify-center">
+          <InputBox
+            focus
+            value={inputUrl}
+            onChange={handleChangeInputUrl}
+            onKeyPress={handleKeyPress}
+            placeholder="Github Repository URL 입력"
+          />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
