de4518e29a4f9f775d76b7474fc89fcc75df6da8
degurii <sansam709@gmail.com>
2022-03-08 01:00:06 +0900

    feat: InputBox 컴포넌트 구현
    * value state, 키보드 이벤트 핸들러와 함께 동작하도록 구현
    
    * 정해진 디자인이 없기 때문에 className을 커스터마이징 할 수 있도록 구현함

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useFocus } from '../../hooks';
+import { classNames } from '../../utils/tailwind';
 
-function InputBox() {
+interface Props {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  value: string;
+  placeholder?: string;
+  focus?: boolean;
+  className?: string;
+}
+function InputBox({
+  focus,
+  onChange,
+  onKeyPress,
+  value,
+  placeholder,
+  className: cn,
+}: Props) {
+  const focusRef = useFocus();
   return (
     <input
+      ref={focus ? focusRef : undefined}
+      value={value}
       type="text"
       name="search"
-      placeholder="Github Repository URL 입력"
-      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 w-[50rem] pr-12 border-gray-300 rounded-md"
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+      className={classNames(
+        'shadow-sm focus:ring-indigo-500 focus:border-indigo-500 w-[50rem] pr-12 border-gray-300 rounded-md',
+        cn,
+      )}
+      placeholder={placeholder}
     />
   );
 }
