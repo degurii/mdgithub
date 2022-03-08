8d22b5bb17f8cccc22f030ca0f3c4c29e7acfa52
degurii <sansam709@gmail.com>
2022-03-09 02:07:20 +0900

    githubClient 에러 로그 출력하도록 변경

--- src/apis/httpClient/githubClient.ts
+++ src/apis/httpClient/githubClient.ts
@@ -11,6 +11,7 @@ class GithubClient extends HttpClient {
     super(config);
 
     this.instance.interceptors.response.use(undefined, (err) => {
+      console.error(err);
       return Promise.reject(err);
     });
   }
