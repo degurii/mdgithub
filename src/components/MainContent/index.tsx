129e4c36fc525379640ca2b10e98891b27a5d44f
degurii <sansam709@gmail.com>
2022-03-09 23:06:12 +0900

    refactor: MainContent 컴포넌트의 역할 변경
    * currentNode에 따라 글 목록, 또는 글을 렌더링 함
    
    * article 페칭을 컴포넌트 내에서 진행하도록 함

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,10 +1,63 @@
+import { GitNode, GitTree, RepoParams } from '../../pages/TIL';
+import ArticleList from '../ArticleList';
+import Article from '../Article';
+import { useAsync } from '../../hooks';
+import { getBlobs, postMarkdown } from '../../apis/services/github';
+import { decode } from 'js-base64';
+import { useEffect, useState } from 'react';
+
 type Props = {
-  children: React.ReactNode;
+  createBlobUrl: (path: string) => string;
+  currentNode?: GitNode;
+  repoParams?: RepoParams;
 };
-function MainContent({ children }: Props) {
+
+export type ArticleData = {
+  text: string;
+  type: 'html' | 'markdown';
+};
+
+function MainContent({ currentNode, repoParams, createBlobUrl }: Props) {
+  const [articleState] = useAsync(
+    async function fetchArticle() {
+      if (!repoParams || !currentNode) {
+        return undefined;
+      }
+      if (currentNode.type === 'blob') {
+        const blob = currentNode;
+        return getBlobs(repoParams.owner, repoParams.repo, blob.sha)
+          .then(({ data }) => decode(data.content))
+          .then(postMarkdown)
+          .then(({ data: articleHtml }) => articleHtml);
+      }
+    },
+    [currentNode, repoParams],
+  );
+  const [article, setArticle] = useState<ArticleData | undefined>();
+
+  useEffect(() => {
+    if (!articleState || articleState.loading) {
+      return;
+    }
+    setArticle({
+      text: articleState.data,
+      type: 'html',
+    });
+  }, [articleState]);
+
+  if (!currentNode || !repoParams) {
+    return null;
+  }
   return (
     <main className="flex flex-1 justify-center pt-16 py-6 px-6 lg:px-8">
-      {children}
+      {currentNode.type === 'tree' ? (
+        <ArticleList
+          createBlobUrl={createBlobUrl}
+          currentNode={currentNode as GitTree}
+        />
+      ) : (
+        <Article data={article?.text} type={article?.type} />
+      )}
     </main>
   );
 }
