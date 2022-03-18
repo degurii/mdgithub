63369348d1494a7b0e6983a2a3959be52efaebdc
degurii <sansam709@gmail.com>
2022-03-19 01:52:18 +0900

    feat: [#8] ArticleList 세부 구현
    * MainContent 컴포넌트에서 현재 카테고리 내의 모든 글을 페칭 한 뒤
      ArticleList에 articles라는 prop으로 넘겨준다.
    
    * ArticleList의 Item은 article, url을 prop으로 받아 링크를 구성한다.

--- src/components/ArticleList/Item.tsx
+++ src/components/ArticleList/Item.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { GitBlob } from '../../pages/Repository';
+import { ArticleData } from '../MainContent';
 
 type Props = {
-  blob: GitBlob;
-  createBlobUrl: (path: string) => string;
+  article: ArticleData;
+  url: string;
 };
 
-function Item({ blob, createBlobUrl }: Props) {
-  return <Link to={createBlobUrl(blob.path)}>{blob.name}</Link>;
+function Item({ article, url }: Props) {
+  return (
+    <Link
+      to={url}
+      className="block border-b-2 py-6 last:border-none first:pt-0"
+    >
+      <p className="text-xl font-semibold text-gray-900 truncate">
+        {article.title}
+      </p>
+      <p className="mt-3 text-base text-gray-500 line-clamp-3">
+        {article.data}
+      </p>
+    </Link>
+  );
 }
 
 export default React.memo(Item);
