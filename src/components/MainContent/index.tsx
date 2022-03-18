28a0db5006fcff95f658b0cce396d53feb7543c4
degurii <sansam709@gmail.com>
2022-03-19 01:47:36 +0900

    refactor: [#10] Main 컴포넌트에서 카테고리의 글 목록을 벌크로 페칭하도록 변경
    * 현재 노드가 속한 tree의 모든 blob을 페칭하도록 함:
      1. 글 목록을 보여줄 때 컨텐츠 내의 제목과 내용을 일부 보여줘야하기 때문
      2. 글을 보여줄 때 같은 카테고리 내의 다른 글들을 보여줘야하기 때문(추후 추가 예정)

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,61 +1,105 @@
-import { BranchInfo, GitNode, GitTree } from '../../pages/Repository';
+import { useEffect, useState } from 'react';
+import { BranchInfo, GitNode } from '../../pages/Repository';
+import { decode } from 'js-base64';
+import { useAsync } from '../../hooks';
+import { getBlobs } from '../../apis/services/github';
+
 import ArticleList from '../ArticleList';
 import Article from '../Article';
-import { useAsync } from '../../hooks';
-import { getBlobs, postMarkdown } from '../../apis/services/github';
-import { useEffect, useState } from 'react';
-import { decode } from 'js-base64';
 
 type Props = {
   createBlobUrl: (path: string) => string;
   currentNode?: GitNode;
   branchInfo?: BranchInfo;
+  navigateToNotFound: () => void;
 };
 
 export type ArticleData = {
-  text?: string;
-  type: 'html' | 'markdown';
+  data: string; // should be markdown
+  path: string;
+  title: string | null;
+  url: string;
 };
 
-function MainContent({ currentNode, branchInfo, createBlobUrl }: Props) {
-  const [articleState] = useAsync<string | undefined>(
-    async function fetchArticle() {
-      if (!branchInfo || !currentNode) {
-        return;
-      }
-      if (currentNode.type === 'blob') {
-        return getBlobs(branchInfo.owner, branchInfo.repo, currentNode.sha)
-          .then((res) => decode(res.data.content))
-          .then(postMarkdown)
-          .then(({ data: articleHtml }) => articleHtml);
-      }
-    },
-    [currentNode, branchInfo],
-  );
-  const [article, setArticle] = useState<ArticleData | undefined>();
-
-  useEffect(() => {
-    if (!articleState || articleState.loading) {
-      return;
-    }
-    setArticle({
-      text: articleState.data,
-      type: 'html',
-    });
-  }, [articleState]);
-
-  if (!currentNode || !branchInfo) {
+const getTitleFromMarkdown = (data: string) => {
+  const lines = data.split('\n');
+  const titleLine = lines.find((line) => line.startsWith('# '));
+  if (!titleLine) {
     return null;
   }
+  const title = titleLine.replace(/^#+/, '').trim();
+  return title;
+};
+
+function MainContent({
+  currentNode,
+  branchInfo,
+  createBlobUrl,
+  navigateToNotFound,
+}: Props) {
+  const [{ data: articles, error }] = useAsync<ArticleData[] | null>(
+    async function fetchBlobsInCurrentTree() {
+      if (!branchInfo || !currentNode) {
+        return null;
+      }
+      const targetTree =
+        currentNode.type === 'tree' ? currentNode : currentNode.parent;
+
+      return Promise.all(
+        Object.values(targetTree.blobs).map(async (blob) => {
+          const { data } = await getBlobs(
+            branchInfo.owner,
+            branchInfo.repo,
+            blob.sha,
+          );
+          const markdown = decode(data.content);
+          const title = getTitleFromMarkdown(markdown) ?? blob.name;
+          return {
+            data: markdown,
+            path: blob.path,
+            title,
+            url: createBlobUrl(blob.path),
+          };
+        }),
+      );
+    },
+    [currentNode, branchInfo, createBlobUrl],
+  );
+  const [article, setArticle] = useState<ArticleData | null>(null);
+
+  useEffect(
+    function setCurrentArticle() {
+      if (!currentNode || currentNode.type === 'tree' || !articles) {
+        return;
+      }
+      const currentArticle = articles.find(
+        (article) => article.path === currentNode.path,
+      );
+      if (!currentArticle) {
+        navigateToNotFound();
+        return;
+      }
+      setArticle(currentArticle);
+    },
+    [currentNode, articles, navigateToNotFound],
+  );
+
+  if (error) {
+    return <div>에러</div>;
+  }
+  if (!currentNode || !branchInfo) {
+    return (
+      <div>
+        콘텐트 로딩중 콘텐트 로딩중 콘텐트 로딩중 콘텐트 로딩중 콘텐트 로딩중{' '}
+      </div>
+    );
+  }
   return (
-    <main className="flex flex-1 justify-center pt-16 py-6 px-6 lg:px-8">
+    <main className="flex flex-1 w-screen justify-center pt-6 lg:pt-14 pb-6 px-6 lg:px-8">
       {currentNode.type === 'tree' ? (
-        <ArticleList
-          createBlobUrl={createBlobUrl}
-          currentNode={currentNode as GitTree}
-        />
+        <ArticleList articles={articles} />
       ) : (
-        <Article data={article?.text} type={article?.type} />
+        <Article article={article} />
       )}
     </main>
   );
