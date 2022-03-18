83c2fa84302282e1773a8826434a3d8f169833a9
degurii <sansam709@gmail.com>
2022-03-18 15:10:46 +0900

    refactor: [#10] Article 컴포넌트가 마크다운만을 렌더링 하도록 변경
    * github api로 마크다운이 렌더링된 html 파일을 받아오는 방식에서
      그냥 마크다운을 써드 파티 라이브러리로 바로 렌더링 하는 방식을 채택함
    
    * 그에 따라 data만을 prop으로 받는 것으로 변경

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,36 +1,21 @@
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import reamarkGfm from 'remark-gfm';
 
 type Props = {
   data?: string;
-  type?: 'markdown' | 'html';
 };
 
-function Article({ data, type }: Props) {
+function Article({ data }: Props) {
   if (!data) {
     return <div>로딩중</div>;
-    // return (
-    //   <div className="w-60 h-24 border-2 rounded-md mx-auto mt-20">
-    //     <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-    //       <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-    //       <div className="flex flex-col space-y-3">
-    //         <div className="w-36 bg-gray-300 h-6 rounded-md "></div>
-    //         <div className="w-24 bg-gray-300 h-6 rounded-md "></div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
   }
 
-  return type === 'markdown' ? (
-    <article className="prose prose-slate break-all w-full">
-      <ReactMarkdown>{data}</ReactMarkdown>
+  return (
+    <article className="prose break-word w-screen">
+      <ReactMarkdown remarkPlugins={[reamarkGfm]}>{data}</ReactMarkdown>
     </article>
-  ) : (
-    <article
-      className="prose prose-stone break-all w-full"
-      dangerouslySetInnerHTML={{ __html: data }}
-    />
   );
 }
 
-export default Article;
+export default React.memo(Article);
